f06384870d32f2f2b6dbc81431ca868b04825e3b
David Garcia <david.garcia@grupomost.com>
2026-04-14 21:27:46 -0300

    ref : modidifcate file

--- ConvertPngToPdf/app/src/main/java/com/nirv/converttopdf/ui/files/DirectoryFile.kt
+++ ConvertPngToPdf/app/src/main/java/com/nirv/converttopdf/ui/files/DirectoryFile.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import org.koin.androidx.compose.koinViewModel
 
@@ -15,5 +16,6 @@ fun DirectoryPdf(
 ){
     // Observamos el stateFlow del ViewModel
     val pdfFiles by viewModel.pdfFiles.collectAsState()
+
     
 }
